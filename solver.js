8dd3e0671bb51ac813e0a22ebd1a143828660ea3
David Molinet <dmolinet@nexusgeographics.com>
2021-12-26 19:57:18 +0100

    Date bugfix

--- solver.js
+++ solver.js
@@ -1,6 +1,5 @@
-var today = new Date();
 var request = new XMLHttpRequest();
-request.open('get', `https://paraulogic.rodamots.cat?solucions=${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`);
+request.open('get', `https://paraulogic.rodamots.cat?solucions=${new Date().toISOString().substr(0, 10).replaceAll('/', '-')}`);
 request.setRequestHeader('Authorization', 'Basic Y29udHJhc2VueWE=');
 request.onreadystatechange = (result) => {
   if (result.currentTarget.readyState === 4 && result.currentTarget.status === 200) {
